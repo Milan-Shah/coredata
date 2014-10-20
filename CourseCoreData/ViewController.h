2d959a827007e9c775829abdb16dc60174af86d0
Milan Shah <mshah1992@live.com>
2014-10-20 11:08:06 -0400

    Initial Commit

---
+++ CourseCoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CourseCoreData
+//
+//  Created by Milan Shah on 10/20/14.
+//  Copyright (c) 2014 Milan Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CourseCoreData/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CourseCoreData
+//
+//  Created by Milan Shah on 10/20/14.
+//  Copyright (c) 2014 Milan Shah. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
